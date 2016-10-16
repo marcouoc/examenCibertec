48b6b5a864145162768defaeab34102b404513a8
Luis Enrique <luchoarrieta@hotmail.com>
2016-10-16 08:58:30 -0500

    nuevo proyecto

---
+++ WebExamenDoFactory/Areas/DoFactoryBD/DoFactoryBDAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace WebExamenDoFactory.Areas.DoFactoryBD
+{
+    public class DoFactoryBDAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "DoFactoryBD";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "DoFactoryBD_default",
+                "DoFactoryBD/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
